dd0cd8a29e135228a42cd2ca852b1fcd10c46df0
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-19 10:26:31 +0100

    Create nav component

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function nav () {
+    return (
+        <div>
+            <ul>
+                <li> Home </li>
+                <li> About Us </li>
+                <li> Contact </li>
+            </ul>
+        </div>
+    )
+}
+
+export default nav
