25737e091bfb461d1171f252be79ba937ff182eb
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-20 10:52:44 +0100

    Update navigation menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import logo from '../img/logo.jpg';
 
-function nav () {
+function Nav () {
     return (
-        <div>
-            <ul>
-                <li> Home </li>
-                <li> About Us </li>
-                <li> Contact </li>
-            </ul>
+        <div className='relative container mx-auto p-6 '>
+            <div className='flex items-center justify-between'>
+                <div className='pt-2'>
+                    <img src={logo} alt='bird'/>
+                </div>
+                <div className='flex space-x-6'>
+                    <ul>
+                        <li> Home </li>
+                        <li> About Us </li>
+                        <li> Pricing </li>
+                        <li> Contact </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
-}
+};
 
-export default nav
+export default Nav
