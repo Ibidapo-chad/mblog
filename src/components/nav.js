b2ce14fdd661cbe316f13ff0380e82506eff7a8b
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-21 11:06:59 +0100

    Update navigation styles

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import logo from '../img/logo.jpg';
+import logo from '../img/Logo.png';
 
 function Nav () {
     return (
-        <div className='relative container mx-auto p-6 '>
+        <div className='relative container mx-auto p-4 font-medium'>
             <div className='flex items-center justify-between'>
-                <div className='pt-2'>
+                {/* Image container */}
+                <div className='pt-2 w-1/5'>
                     <img src={logo} alt='bird'/>
                 </div>
-                <div className='flex space-x-6'>
-                    <ul>
-                        <li> Home </li>
-                        <li> About Us </li>
-                        <li> Pricing </li>
-                        <li> Contact </li>
-                    </ul>
+                {/* Nav Menu container*/}
+                <div className='hidden md:flex space-x-6'>
+                    <a href='#'className='hover:text-darkGrayishBlue'>Home</a>
+                    <a href='#'className='hover:text-darkGrayishBlue'>About us</a>
+                    <a href='#'className='hover:text-darkGrayishBlue'>Pricing</a>
+                    <a href='#'className='hover:text-darkGrayishBlue'>Contact Us</a>
                 </div>
+                {/* Button */}
+                <a href='#' className='hidden md:block p-3 px-6 text-white rounded-full align-baseline bg-brightRed hover:bg-brightRedLight'>
+                    Get Started
+                </a>
             </div>
         </div>
     )
