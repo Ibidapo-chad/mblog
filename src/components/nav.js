a16cba1fafef967b22a3758389216888bfc36f9a
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-23 23:59:38 +0100

    Refactor Nav-II bar in new comp

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from '../img/Logo.png';
+import HamMenuButton from './HamMenuButton';
 
 function Nav () {
     return (
-        <div className='relative container mx-auto p-4 font-medium'>
+        <nav className='flex container mx-auto p-4 font-medium'>
             <div className='flex items-center justify-between'>
                 {/* Image container */}
                 <div className='pt-2 w-1/5'>
@@ -20,8 +21,15 @@ function Nav () {
                 <a href='#' className='hidden md:block p-3 px-6 text-white rounded-full align-baseline bg-brightRed hover:bg-brightRedLight'>
                     Get Started
                 </a>
+                {/* Hamburger Icon */}
+                <div>
+                    <HamMenuButton />
+                </div>
+
             </div>
-        </div>
+
+            {/* Mobile Menu */}
+        </nav>
     )
 };
 
