e4cdc6aa247ee1b9e2b6fe726fc580524dbfb0c5
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-24 00:00:00 +0100

    Update App comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Nav from './components/nav.js';
+import Nav from './components/nav';
 import Hero from './components/Hero';
+import Features from './components/Features.js';
+import Testimonial from './components/Testimonial.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Hero />
+      <Features />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
