d499d816b7e4b3563df9bc7c4f3f684344886c0f
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-24 00:04:16 +0100

    Add new color to palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
+        royalBlue: '#1E366C',
       },
     },
   },
