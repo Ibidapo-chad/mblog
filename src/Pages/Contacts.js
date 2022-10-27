0ef7e5ceb01c633ae4f5b12b483c2513c4269815
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-27 21:01:05 +0100

    Add the pages route into App

---
+++ src/Pages/Contacts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contacts() {
+    return (
+        <>
+            <h1>Hi, I am Contacts. I am your go to guy.</h1>
+            <h2>I am expensive but I am currently 
+                working to cut down so you can afford me.
+            </h2>
+        </>
+    )
+};
+
+export default Contacts
---
+++ src/Pages/Pricing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Pricing() {
+    return (
+        <>
+            <h1>Hi, I am Pricing. I govern this page.</h1>
+            <h2>I am still under construction. I do apologize for the inconvenience I have caused you.</h2>
+        </>
+    )
+};
+
+export default Pricing
