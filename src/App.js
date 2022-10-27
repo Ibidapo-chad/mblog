1af1716cccda984bebdbf8614a2e9acced92901a
ibidapo-chad <ibidapo.chad@gmail.com>
2022-10-27 21:02:39 +0100

    Add both Routes and Route components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import Nav from './components/nav';
+import Nav from './components/Nav';
 import Hero from './components/Hero';
 import Features from './components/Features.js';
 import Testimonial from './components/Testimonial.js';
 import Footer from './components/Footer.js';
+import {Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Pricing from './Pages/Pricing';
+import Contacts from './Pages/Contacts';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Routes>
+        <Route path='/' element= {<Home />} />
+        <Route path='/pricing' element= {<Pricing />} />
+        <Route path='/contacts' element= {<Contacts />} />
+      </Routes>
+
       <Nav />
       <Hero />
       <Features />
       <Testimonial />
       <Footer />
-    </div>
+    </>
   );
 }
 
